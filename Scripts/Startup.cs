2023e27aa8b4d29e57077bb4f999bfe0f5c40c1d
douglas-snyder <mistershake2099@gmail.com>
2025-07-31 18:23:48 -0400

    Change scene Main to Main_Menu. Add Startup_Scene for init.

--- Scripts/Startup.cs
+++ Scripts/Startup.cs
@@ -6,6 +6,10 @@ public partial class Startup : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// Load initial scene (loading screen, login screen, or Main_Menu?)
+
+		// Load player-specific data
+
 		return;
 	}
 
