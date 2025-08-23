eb8694d9fd700754305652943dcb3c62630b0bdd
douglas-snyder <mistershake2099@gmail.com>
2025-08-22 23:29:11 -0400

    Remove non-static references.

--- Scripts/PlayerData.cs
+++ Scripts/PlayerData.cs
@@ -3,34 +3,36 @@ using System;
 
 public partial class PlayerData : ConfigFile
 {
+	ConfigFile config = new ConfigFile();
+
 	static void CreatePlayerData()
 	{
-		this.SetValue("Player", "PlayerName", "Rock Person");
-		this.SetValue("Nation", "NationName", "Planet Rock");
-		this.SetValue("Nation", "NationRulerName", "Ruler of Planet Rock");
-		this.SetValue("Nation", "NationPower", 9001);
+		config.SetValue("Player", "PlayerName", "Rock Person");
+		config.SetValue("Nation", "NationName", "Planet Rock");
+		config.SetValue("Nation", "NationRulerName", "Ruler of Planet Rock");
+		config.SetValue("Nation", "NationPower", 9001);
 
-		this.Save("user://PlayerData.cfg");
+		config.Save("user://PlayerData.cfg");
 	}
 
 	// Accept a Dictionary or return a Dictionary?
 	static void LoadPlayerData()
 	{
-		Error loadPlayerDataError = this.Load("user://PlayerData.cfg");
+		Error loadPlayerDataError = config.Load("user://PlayerData.cfg");
 		if (loadPlayerDataError != Error.Ok) { return; }
 
-		foreach (String section in this.GetSections())
+		foreach (String section in config.GetSections())
 		{
 			// Change this to use fetched section headers or similar
-			var playerName = (String)this.GetValue(section, "PlayerName");
-			var nationName = (String)this.GetValue(section, "NationName");
-			var nationRulerName = (String)this.GetValue(section, "NationRulerName");
-			var nationPower = (int)this.GetValue(section, "NationPower");
+			var playerName = (String)config.GetValue(section, "PlayerName");
+			var nationName = (String)config.GetValue(section, "NationName");
+			var nationRulerName = (String)config.GetValue(section, "NationRulerName");
+			var nationPower = (int)config.GetValue(section, "NationPower");
 		}
 	}
 
 	static void SavePlayerData()
 	{
-		this.Save("user://PlayerData.cfg");
+		config.Save("user://PlayerData.cfg");
 	}
 }
