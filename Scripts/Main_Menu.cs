96a07b545006c8db9d5da744b8ae7da21486642a
douglas-snyder <mistershake2099@gmail.com>
2025-09-28 23:42:55 -0400

    Add new Scenes and some placeholder scripts for them.

--- Scripts/Main_Menu.cs
+++ Scripts/Main_Menu.cs
@@ -6,7 +6,11 @@ public partial class Main_Menu : Control
 
 	public override void _Ready()
 	{
-		GetNode<Button>("City_Name").Text = "Test";
+		GetNode<Button>("Nation_Panel").Text = "Nation Panel Test";
+		GetNode<Button>("City_Name").Text = "City Name Test";
+		GetNode<Button>("Social_Panel").Text = "Social Panel Test";
+		GetNode<Button>("City_Management").Text = "City Management Test";
+		GetNode<Button>("News_Panel").Text = "News Panel Test";
 	}
 
 }
