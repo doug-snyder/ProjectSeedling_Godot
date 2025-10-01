73bdd9cf86fa64adbb5e03a43001be72ed93b94f
douglas-snyder <mistershake2099@gmail.com>
2025-09-30 22:53:27 -0400

    Add button press signaling.

--- Scripts/Main_Menu.cs
+++ Scripts/Main_Menu.cs
@@ -3,14 +3,53 @@ using System;
 
 public partial class Main_Menu : Control
 {
+	Button nationButton;
+	Button cityButton;
+	Button socialButton;
+	Button managementButton;
+	Button newsButton;
 
 	public override void _Ready()
 	{
-		GetNode<Button>("Nation_Panel").Text = "Nation Panel Test";
-		GetNode<Button>("City_Name").Text = "City Name Test";
-		GetNode<Button>("Social_Panel").Text = "Social Panel Test";
-		GetNode<Button>("City_Management").Text = "City Management Test";
-		GetNode<Button>("News_Panel").Text = "News Panel Test";
+		nationButton = GetNode<Button>("Nation_Panel");
+		nationButton.Text = "Nation Panel Test";
+		nationButton.Pressed += nationButtonPressed;
+
+		cityButton = GetNode<Button>("City_Name");
+		cityButton.Text = "City Name Test";
+		cityButton.Pressed += cityButtonPressed;
+
+		socialButton = GetNode<Button>("Social_Panel");
+		socialButton.Text = "Social Panel Test";
+		socialButton.Pressed += socialButtonPressed;
+
+		managementButton = GetNode<Button>("City_Management");
+		managementButton.Text = "City Management Test";
+		managementButton.Pressed += managementButtonPressed;
+
+		newsButton = GetNode<Button>("News_Panel");
+		newsButton.Text = "News Panel Test";
+		newsButton.Pressed += newsButtonPressed;
+	}
+
+	private void nationButtonPressed() {
+		GD.Print("Nation Button Pressed");
+	}
+
+	private void cityButtonPressed() {
+		GD.Print("City Button Pressed");
+	}
+
+	private void socialButtonPressed() {
+		GD.Print("Social Button Pressed");
+	}
+
+	private void managementButtonPressed() {
+		GD.Print("Management Button Pressed");
+	}
+
+	private void newsButtonPressed() {
+		GD.Print("News Button Pressed");
 	}
 
 }
