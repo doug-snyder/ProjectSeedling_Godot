99245ef9035acb2be45eb81d65facd56e0bd2da0
douglas-snyder <mistershake2099@gmail.com>
2025-10-01 23:53:43 -0400

    Add initial scene-changing functionality.

--- Scripts/Main_Menu.cs
+++ Scripts/Main_Menu.cs
@@ -9,6 +9,12 @@ public partial class Main_Menu : Control
 	Button managementButton;
 	Button newsButton;
 
+	PackedScene nationScene = GD.Load<PackedScene>("res://Scenes/Nation_Scene.tscn");
+	PackedScene cityScene = GD.Load<PackedScene>("res://Scenes/City_Scene.tscn");
+	PackedScene socialScene = GD.Load<PackedScene>("res://Scenes/Social_Scene.tscn");
+	PackedScene managementScene = GD.Load<PackedScene>("res://Scenes/Management_Scene.tscn");
+	PackedScene newsScene = GD.Load<PackedScene>("res://Scenes/News_Scene.tscn");
+
 	public override void _Ready()
 	{
 		nationButton = GetNode<Button>("Nation_Panel");
@@ -34,22 +40,32 @@ public partial class Main_Menu : Control
 
 	private void nationButtonPressed() {
 		GD.Print("Nation Button Pressed");
+		var nationInstance = nationScene.Instantiate();
+		AddChild(nationInstance);
 	}
 
 	private void cityButtonPressed() {
 		GD.Print("City Button Pressed");
+		var cityInstance = cityScene.Instantiate();
+		AddChild(cityInstance);
 	}
 
 	private void socialButtonPressed() {
 		GD.Print("Social Button Pressed");
+		var socialInstance = socialScene.Instantiate();
+		AddChild(socialInstance);
 	}
 
 	private void managementButtonPressed() {
 		GD.Print("Management Button Pressed");
+		var managementInstance = managementScene.Instantiate();
+		AddChild(managementInstance);
 	}
 
 	private void newsButtonPressed() {
 		GD.Print("News Button Pressed");
+		var newsInstance = newsScene.Instantiate();
+		AddChild(newsInstance);
 	}
 
 }
